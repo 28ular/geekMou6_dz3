44579cad3ca3511607476fff2b5ef366297539aa
admin <ularmateev27@gmail.com>
2025-10-19 01:05:39 +0600

    dfsf

--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,9 @@ import App from './App.jsx';
 import { RouterProvider } from "react-router-dom";
 import { route } from "./route.jsx";
 
-import manifest from './manifest.json';
-import icon192 from './assets/icon-192.png';
-import icon512 from './assets/icon-512.png';
 
 createRoot(document.getElementById('root')).render(
     <RouterProvider router={route}>
         <App />
     </RouterProvider>
 );
-
-// Service Worker регистрация для PWA
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/sw.js")
-            .then((reg) => console.log("SW registered!", reg))
-            .catch((err) => console.log("SW registration failed:", err));
-    });
-}
